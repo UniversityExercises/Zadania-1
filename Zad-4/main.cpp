3f5e9c42556964bfae735275b015bef02bf01496
KrzysiekBigaj <krzysiek.bigaj@wp.pl>
2011-11-03 22:36:05 +0100

    Sprawdz wartosc

---
+++ Zad-4/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    cout<<"wprowadz liczbe"<<endl;
+    cin>>x;
+
+    if(x>=0)
+    cout<<"liczba dodatnia";
+    else
+    cout<<"liczba ujemna";
+    return 0;
+}
