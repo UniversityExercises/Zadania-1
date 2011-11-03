35063f3a048572057906d244c2e94c6441c50841
KrzysiekBigaj <krzysiek.bigaj@wp.pl>
2011-11-03 22:33:48 +0100

    Dzielenie

---
+++ Zad-3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m, n;
+
+    cout<<"wprowadz m i n\n";
+
+    cin>>m>>n;
+
+    cout<<"wynik="<<m/n<<" reszta "<<m%n;
+
+    return 0;
+}
