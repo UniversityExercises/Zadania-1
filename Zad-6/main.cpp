26c73b74fa22c92c6991fe4a41cc029412c054cf
KrzysiekBigaj <krzysiek.bigaj@wp.pl>
2011-11-03 22:40:42 +0100

    liczba dwucyfrowa

---
+++ Zad-6/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"wprowadz jedna liczbe"<<endl;
+    cin>>a;
+    if(a>=10 && a<100)
+    cout<<"dodatnia dwucyfrowa";
+    else
+    cout<<"inna";
+    return 0;
+}
